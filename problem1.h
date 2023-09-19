21681057f90f699182452c3d4a92f9b48447206e
fizban007 <fizban007@gmail.com>
2023-09-18 21:57:06 -0500

    updated comments

--- problem1.h
+++ problem1.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <tuple>
 #include <cmath>
 #include <iostream>
+#include <tuple>
 
 template <typename F>
-double trapezoidal_next(const F& f, double a, double b, double dx, double S) {
+double trapezoidal_next(const F &f, double a, double b, double dx, double S) {
   double sum = 0.0;
+  // the first extra point is at 0.5 * dx from a, rest are dx apart
   for (double x = a + 0.5 * dx; x < b; x += dx) {
     sum += f(x);
   }
@@ -14,23 +15,29 @@ double trapezoidal_next(const F& f, double a, double b, double dx, double S) {
 }
 
 template <typename F>
-std::tuple<bool, double> adaptive_simpson(const F& f, double a, double b, double tolerance) {
+std::tuple<bool, double> adaptive_simpson(const F &f, double a, double b,
+                                          double tolerance) {
   double h = b - a;
 
+  // First trapezoidal estimate
   double S = 0.5 * h * (f(a) + f(b));
-  double I = S;
+  double Sprime = S;
 
-  for (int n = 1; n < 100; n++) {
-    std::cout << n << " " << I << std::endl;
+  for (int n = 1; n < 30; n++) {
+    // std::cout << n << " " << Sprime << std::endl;
+    // get the next trapezoidal estimate
     double Snext = trapezoidal_next(f, a, b, h, S);
-    double Inext = (4.0 * Snext - S) / 3.0;
+    // construct the next Simpson estimate
+    double Sprime_next = (4.0 * Snext - S) / 3.0;
 
-    if (std::abs((Inext - I) / Inext) / 15.0 < tolerance) {
-      return std::make_tuple(true, Inext);
+    // check for convergence
+    if (std::abs((Sprime_next - Sprime) / Sprime_next) / 15.0 < tolerance) {
+      return std::make_tuple(true, Sprime_next);
     }
-    I = Inext;
+    // update for next iteration
+    Sprime = Sprime_next;
     S = Snext;
     h *= 0.5;
   }
-  return std::make_tuple(false, I);
+  return std::make_tuple(false, Sprime);
 }
--- problem2.h
+++ problem2.h
@@ -4,12 +4,14 @@
 
 template <typename F>
 double gauss_quadrature_10(const F &f, double a, double b) {
+  // abscissas and weights for 10-point Gauss quadrature
   const double x[5] = {0.1488743389816312, 0.4333953941292472,
                        0.6794095682990244, 0.8650633666889845,
                        0.9739065285171717};
   const double w[5] = {0.2955242247147529, 0.2692667193099963,
                        0.2190863625159821, 0.1494513491505806,
                        0.0666713443086881};
+  // this is the solution in Numerical Recipes
   double xm = 0.5 * (b + a);
   double xr = 0.5 * (b - a);
   double s = 0.0;

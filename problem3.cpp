da444b71a39e2b73ed885b7aca5c4bad6469548c
fizban007 <fizban007@gmail.com>
2023-09-18 21:59:28 -0500

    fixed original typos in the repo

--- problem3.cpp
+++ problem3.cpp
@@ -1,5 +1,5 @@
+#include "problem1.h"
 #include "problem2.h"
-#include "problem3.h"
 
 #include <iostream>
 
@@ -39,13 +39,13 @@ int main() {
   t = gauss_quadrature_10(f5, 0.0, M_PI * 0.5);
   std::cout << "th_max = pi / 2: t/t0 = " << t * 2.0 / M_PI << std::endl;
 
-  th_max = 179.0 / 180.0 * M_PI;
-  k = std::sin(th_max * 0.5);
-  auto f6 = [k](double x) { return 1.0 / std::sqrt(1.0 - k*k*std::pow(std::sin(x), 2)); };
-  // t = gauss_quadrature_10(f6, 0.0, M_PI * 0.5);
-  auto result = adaptive_simpson(f6, 0.0, M_PI * 0.5, 1e-12);
-  t = std::get<1>(result);
-  std::cout << "th_max = 179^o: t/t0 = " << t * 2.0 / M_PI << std::endl;
+  // th_max = 179.0 / 180.0 * M_PI;
+  // k = std::sin(th_max * 0.5);
+  // auto f6 = [k](double x) { return 1.0 / std::sqrt(1.0 - k*k*std::pow(std::sin(x), 2)); };
+  // // t = gauss_quadrature_10(f6, 0.0, M_PI * 0.5);
+  // auto result = adaptive_simpson(f6, 0.0, M_PI * 0.5, 1e-12);
+  // t = std::get<1>(result);
+  // std::cout << "th_max = 179^o: t/t0 = " << t * 2.0 / M_PI << std::endl;
 
   return 0;
 }
